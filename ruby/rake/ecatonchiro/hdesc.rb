738fa448f21ee7431229b0e53eb633ca360fc1f0
Domenico Chierico <spaghetty@gmail.com>
2010-06-28 13:55:18 +0200

    add some stuf

---
+++ ruby/rake/ecatonchiro/hdesc.rb
@@ -0,0 +1,44 @@
+
+class HostDescriptor
+  attr_accessor :ip, :uname, :path, :type
+  def initialize(type, ip, uname)
+    @type = type.strip()
+    @ip = ip.strip()
+    @uname = uname.strip()
+    has_key = false
+  end
+  
+  def has_key?
+    return @has_key
+  end
+
+  def need_sudo?
+    return (not @uname=="root")
+  end
+  
+  def has_hey=(val)
+    @has_key = val
+  end
+
+  def get_ssh_string()
+    return "#{@uname}@#{@ip}"
+  end
+
+  def get_key_file()
+    return "#{GIT_REPO}/#{ip}/.ssh_key"
+  end
+
+  def self.find_by_ip(ip)
+    found = nil
+    if File.exists? "#{GIT_REPO}/#{ip}/.ecatonchiro"
+      f = File.open("#{GIT_REPO}/#{ip}/.ecatonchiro", "r")
+      desc = f.gets()
+      el = desc.split(":")
+      found = HostDescriptor.new(el[1], el[0], el[2])
+      if File.exists? "#{GIT_REPO}/#{ip}/.ssh_key"
+        found.has_key = true
+      end
+    end
+    return found
+  end
+end
